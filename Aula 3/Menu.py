a95ea8b1ee3f73be8ccefadd21f9cff7223e69d3
joao vitor marchetti <marchettijoao2@gmail.com>
2026-03-04 20:29:54 -0300

    Add files via upload

---
+++ Aula 3/Menu.py
@@ -0,0 +1,37 @@
+from Sistema import Sistema
+
+sistema = Sistema()
+
+while True:
+    print("1.Cadastrar Aluno")
+    print("2.Listar Alunos")
+    print("3.Cadastrar Tarefa")
+    print("4.Listar Tarefa")
+    print("5.Concluir Tarefa")
+    print("0. Sair")
+
+    opcao = input("Escolha uma Opção :")
+
+    if opcao == "1" :
+        nome = input("Digite o nome do aluno: ")
+        sistema.cadastrarAluno(nome)
+    elif opcao == "2":
+        sistema.listarAlunos()
+    elif opcao ==  "3":
+       idAluno = int(input("ID do Aluno: "))
+       descricao = input("Descrição da Tarefa: ")
+       sistema.cadastrarTarefa(idAluno, descricao)
+    elif opcao == "4" :
+        idAluno = int(input("ID do Aluno: "))
+        sistema.listarTarefa(idAluno)
+    elif opcao == "5" :
+        idAluno = int(input("ID do Aluno: "))
+        idTarefa = int(input("ID da Tarefa: "))
+        sistema.concluirTarefa(idAluno, idTarefa)
+    elif opcao =="0" :
+        break
+    else:
+        print("Opcão Invalida.")
+  
+   
+
