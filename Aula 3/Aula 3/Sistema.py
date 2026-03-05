7e6f128a5efa448b740c37de84f83cb2460d4069
joao vitor marchetti <marchettijoao2@gmail.com>
2026-03-04 21:51:45 -0300

    Add files via upload

---
+++ Aula 3/Aula 3/Sistema.py
@@ -0,0 +1,50 @@
+from Aluno import Aluno
+from Tarefa import Tarefa
+
+class Sistema:
+    def __init__(self):
+        self.alunos = []
+        self.contadorAluno = 1
+        self.contadorTarefa =1
+
+    def cadastrarAluno(self, nome):
+        aluno = Aluno(self.contadorAluno, nome)
+        self.alunos.append(aluno)
+        self.contadorAluno += 1
+        print("Aluno cadastrado com sucesso!")
+
+    def listarAlunos(self):
+        if not self.alunos:
+            print("Nenhum aluno cadastrado")
+            return
+        
+        for aluno in self.alunos:
+            print(aluno)
+
+
+    def castrarTarefa(self, idAluno, descricao):
+        for aluno in self.alunos:
+            if aluno.id == idAluno:
+                tarefa = Tarefa(self.contadorAluno, descricao)
+                aluno.adicionarTarefa(tarefa)
+                self.contadorTarefa += 1
+                print("Tarefa cadastrada!")
+                return
+        print("Aluno não encontrado!")
+
+
+    def listarTarefas(self, idAluno):
+        for aluno in self.aluno:
+            aluno.listarTarefas()
+            return
+        print("Aluno não encontrado.")
+
+    def concluirTarefa(self, idAluno, idTarefa):
+        for aluno in self.alunos:
+            if aluno.id == idAluno:
+                for tarefa in aluno.tarefas:
+                    if tarefa.id == idTarefa:
+                        tarefa.concluida = True
+                        print("Tarefa Concluida: ")
+                        return
+        print("Tarefa não encontrada: ")            
