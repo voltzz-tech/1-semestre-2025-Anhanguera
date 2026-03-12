2bbc078be0ef2f871e68b8cb41818b082f66d8cd
joao vitor marchetti <marchettijoao2@gmail.com>
2026-03-11 21:44:52 -0300

    Delete Aula 3 directory

--- Aula 3/Tarefa.py
+++
@@ -1,9 +0,0 @@
-class Tarefa:
-    def __init__(self, id, descricao):
-        self.id = id
-        self.descricao = descricao
-        self.concluida = False
-
-    def __str__(self):
-        status = "OK" if self.concluida else "x"
-        return f"{self.id} - {self.descricao} [{status}]"
